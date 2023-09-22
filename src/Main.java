2640f46fe6e00382afe69a615c325d6b428d2419
Admiral <pasechnik-95@yandex.ru>
2023-09-22 21:26:18 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+        do {
+            i--;
+        } while (i > 1);
+        System.out.println("Value i = " + i);
+    }
+}
